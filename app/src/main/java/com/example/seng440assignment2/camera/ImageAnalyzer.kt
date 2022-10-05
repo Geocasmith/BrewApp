a9514b664a75a6693acf689d9552f1d2a0541acc
Samuel Burtenshaw <sgb79@uclive.ac.nz>
2022-10-05 16:04:16 +1300

    Setup Basic Scanning Screen and Barcode analysiser

---
+++ app/src/main/java/com/example/seng440assignment2/camera/ImageAnalyzer.kt
@@ -0,0 +1,152 @@
+package com.example.seng440assignment2.camera
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.util.Log
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Switch
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.text.Text
+import com.google.mlkit.vision.text.TextRecognition
+import com.google.mlkit.vision.text.latin.TextRecognizerOptions
+
+
+class TextAnalyzer(private val onImageScanned: (Text) -> Unit ): ImageAnalysis.Analyzer {
+
+    private val recognizer = TextRecognition.getClient(TextRecognizerOptions.DEFAULT_OPTIONS)
+
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            recognizer.process(image)
+                .addOnSuccessListener { onImageScanned(it) }
+                .addOnCompleteListener { imageProxy.close() }
+
+        }
+    }
+
+}
+
+
+class BarcodeAnalyzer(private val onBarcodeScanned: (List<Barcode>?) -> Unit ): ImageAnalysis.Analyzer {
+    private val scanner = BarcodeScanning.getClient()
+
+    @SuppressLint("UnsafeOptInUsageError")
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            scanner.process(image)
+                .addOnSuccessListener { onBarcodeScanned(it) }
+                .addOnCompleteListener { imageProxy.close() }
+        }
+    }
+}
+
+
+
+
+
+@Composable
+fun ScanScreen() {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    var isBarcodeScanning by remember { mutableStateOf(true) }
+
+    val cameraProviderFuture = remember {
+        ProcessCameraProvider.getInstance(context)
+    }
+
+    var hasCamPermission by remember {
+        mutableStateOf(
+            ContextCompat.checkSelfPermission(
+                context,
+                Manifest.permission.CAMERA
+            ) == PackageManager.PERMISSION_GRANTED
+        )
+    }
+
+    val launcher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { granted -> hasCamPermission = granted }
+    )
+
+    LaunchedEffect(key1 = true) {
+        // Check that camera permission has been given
+        launcher.launch(Manifest.permission.CAMERA)
+    }
+
+    val imageAnalysis = ImageAnalysis.Builder()
+        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+        .build()
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        if(hasCamPermission) {
+            Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+                AndroidView(
+                    factory = { context ->
+                        val previewView = PreviewView(context)
+                        val preview = Preview.Builder().build()
+
+                        val selector = CameraSelector.Builder()
+                            .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                            .build()
+                        preview.setSurfaceProvider(previewView.surfaceProvider)
+
+                        imageAnalysis.setAnalyzer(
+                            ContextCompat.getMainExecutor(context),
+                            BarcodeAnalyzer { result ->
+                                /* TODO: Use Barcodes to do something -> Access Database? */
+
+                                result?.forEach {
+                                    Log.i("TAG", "ScanScreen: " + it.rawValue)
+                                }
+                            })
+                        try {
+                            cameraProviderFuture.get().bindToLifecycle(
+                                lifecycleOwner,
+                                selector,
+                                preview,
+                                imageAnalysis
+                            )
+                        } catch (e: Exception) {
+                            e.printStackTrace()
+                        }
+                        previewView
+                    },
+                    modifier = Modifier
+                        .fillMaxSize()
+                )
+                Switch(checked = isBarcodeScanning, onCheckedChange = { isBarcodeScanning = it })
+            }
+        }
+
+    }
+}
