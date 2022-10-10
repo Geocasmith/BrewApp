6433923cc8c5e780f71d26346fa604c5ed4e9f1c
Mitchell Veale <mjv43@uclive.ac.nz>
2022-10-10 16:32:22 +1300

    [api] user register endpoint now works

--- api/app/models/users.model.ts
+++ api/app/models/users.model.ts
@@ -10,14 +10,10 @@ exports.create = async function (user: UserCreate) {
 
     try {
         const pool = await db.getPool();
-        console.log('here');
         const result = await pool.query(sql, data);
-        console.log('here2');
-        console.log(JSON.stringify(result));
-        return result[0].insertId;
+        return result.insertId;
     } catch (e) {
         errorService.logSqlError(e);
-        console.error(e)
         throw e;
     }
 };
