7ce5f36a198b1d9dd2027eb75ac3465f9a2e7447
Samuel Burtenshaw <sgb79@uclive.ac.nz>
2022-10-10 16:44:46 +1300

    Setup for datastore objects

--- app/src/main/java/com/example/seng440assignment2/datastore/UserPreferencesSerializer.kt
+++
@@ -1,8 +0,0 @@
-package com.example.seng440assignment2.datastore
-
-import androidx.datastore.core.Serializer
-import com.example.seng440assignment2.UserPreferences
-
-object UserPreferencesSerializer : Serializer<UserPreferences> {
-
-}
